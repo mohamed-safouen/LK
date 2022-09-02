dceea501721b9968970ad22093f932f88783b4de
mohamed-safouen <mohamedsafouen@gmail.com>
2022-09-02 17:15:53 +0100

    Add video back

--- js/custom.js
+++ js/custom.js
@@ -16,13 +16,20 @@
 
 
     // ABOUT SLIDER
-    $('body').vegas({
-        slides: [
-            { src: 'images/slide-image01.jpg' },
-            { src: 'images/slide-image02.jpg' }
-        ],
-        timer: false,
-        transition: [ 'zoomOut', ]
+    $("body").vegas({
+      delay: 20000,
+      slides: [
+        {
+          src: "images/slide-image02.jpg",
+          video: {
+            src: ["images/Wrath.mp4"],
+            loop: true,
+            mute: true,
+          },
+        },
+      ],
+      timer: false,
+      transition: ["zoomOut"],
     });
 
 })(jQuery);
